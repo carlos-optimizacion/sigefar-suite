ff15a419553e7f7aade4c4973bac6d4e16e785ae
carlos-optimizacion <carlosnias@gmail.com>
2026-06-02 06:30:41 -0500

    Agregar login y logout visuales

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,15 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from core.views import home, module_detail, panel
 
 urlpatterns = [
     path('', home, name='home'),
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('panel/', panel, name='panel'),
     path('modulos/<slug:slug>/', module_detail, name='module_detail'),
     path('admin/', admin.site.urls),
