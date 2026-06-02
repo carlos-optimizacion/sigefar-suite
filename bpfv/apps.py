96c80828bf418998bf899220d83cd416e4dcb066
carlos-optimizacion <carlosnias@gmail.com>
2026-06-02 00:14:14 -0500

    Agrega configuración app bpfv

---
+++ bpfv/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class BpfvConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "bpfv"
+    verbose_name = "SIGEFAR-BPFV / Farmacovigilancia"
