73fef138c61a2d7585204b4fa337eefd878d82cd
carlos-optimizacion <carlosnias@gmail.com>
2026-06-02 21:05:05 -0500

    Redirigir login al inicio del sistema

--- config/settings.py
+++ config/settings.py
@@ -83,7 +83,7 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 LOGIN_URL = "/login/"
-LOGIN_REDIRECT_URL = "/panel/"
+LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
