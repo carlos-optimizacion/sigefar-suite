56a7b67acfbcdd467206394b307b311f8965edb3
carlos-optimizacion <carlosnias@gmail.com>
2026-06-02 21:24:14 -0500

    Usar arquitectura publica con Regulatorio separado de Core

--- config/urls.py
+++ config/urls.py
@@ -4,17 +4,17 @@ from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from core.submodule_views import module_detail_with_submodules, submodule_detail, submodule_index
-from core.views import home, panel
+from core.sigefar_public_views import public_home, public_module_detail, public_submodule_detail, public_submodule_index
+from core.views import panel
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', public_home, name='home'),
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('panel/', panel, name='panel'),
-    path('modulos/<slug:slug>/', module_detail_with_submodules, name='module_detail'),
-    path('modulos/<slug:module_slug>/submodulos/', submodule_index, name='submodule_index'),
-    path('modulos/<slug:module_slug>/<slug:submodule_slug>/', submodule_detail, name='submodule_detail'),
+    path('modulos/<slug:slug>/', public_module_detail, name='module_detail'),
+    path('modulos/<slug:module_slug>/submodulos/', public_submodule_index, name='submodule_index'),
+    path('modulos/<slug:module_slug>/<slug:submodule_slug>/', public_submodule_detail, name='submodule_detail'),
     path('admin/', admin.site.urls),
 ]
 
