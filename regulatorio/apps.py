dec31a4d93f52ef2cfd2d17ddf1ff23790934ad7
carlos-optimizacion <carlosnias@gmail.com>
2026-06-03 00:54:38 -0500

    Add Regulatorio app config

---
+++ regulatorio/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class RegulatorioConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "regulatorio"
+    verbose_name = "SIGEFAR-Regulatorio"
